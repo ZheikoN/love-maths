e798422fe9008386fe78a2c1127051ca987808fe
ZheikoN <negru.inger@gmail.com>
2022-05-24 20:21:25 +0000

    add listeners for page loading and button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,47 @@
-function runGame(){
+// wait for the dom to finish loading 
+// get the button elements and ad event listeners
 
-}
+document.addEventListener("DOMContentLoaded", function() {
+            let buttons = document.getElementsByTagName("button");
+            
+            for (let button of buttons) {
+                button.addEventListener("click", function() {
+                    let gameType = this.getAttribute("data-type");    
+                    if (this.getAttribute("data-type") === "submit") {
+                        alert(`you clicked ${gameType}`);
+                    } else {
+                        alert(`you clicked ${gameType}`);
+                    }
+                })
+            }
+        })
 
-function checkAnswer() {
+        function runGame() {
 
-}
+        }
 
-function calculateCorrectAnswer() {
+        function checkAnswer() {
 
-}
+        }
 
-function incrementScore() {
+        function calculateCorrectAnswer() {
 
-}
+        }
 
-function incrementWrongAnswer() {
+        function incrementScore() {
 
-}
+        }
 
-function displayAdditionQuestion() {
+        function incrementWrongAnswer() {
 
-}
+        }
 
-function displaySubstractQuestion() {
+        function displayAdditionQuestion() {
 
-}
+        }
 
-function displayMultiplyQuestion() {
+        function displaySubstractQuestion() {
 
-}
+        }
+
+        function displayMultiplyQuestion() {}
