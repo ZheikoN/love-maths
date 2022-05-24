0ffa085c9e17e6322171383efdc48a803ce58897
ZheikoN <negru.inger@gmail.com>
2022-05-24 20:27:24 +0000

    add code to calculate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,56 @@
 // wait for the dom to finish loading 
 // get the button elements and ad event listeners
 
-document.addEventListener("DOMContentLoaded", function() {
-            let buttons = document.getElementsByTagName("button");
-            
-            for (let button of buttons) {
-                button.addEventListener("click", function() {
-                    let gameType = this.getAttribute("data-type");    
-                    if (this.getAttribute("data-type") === "submit") {
-                        alert(`you clicked ${gameType}`);
-                    } else {
-                        alert(`you clicked ${gameType}`);
-                    }
-                })
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            let gameType = this.getAttribute("data-type");
+            if (this.getAttribute("data-type") === "submit") {
+                alert(`you clicked ${gameType}`);
+            } else {
+                alert(`you clicked ${gameType}`);
             }
         })
+    }
+})
 
-        function runGame() {
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 
-        }
+function runGame() {
+    // creates two random numbers between 1-25;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
 
-        function checkAnswer() {
+runGame();
 
-        }
+function checkAnswer() {
 
-        function calculateCorrectAnswer() {
+}
 
-        }
+function calculateCorrectAnswer() {
 
-        function incrementScore() {
+}
 
-        }
+function incrementScore() {
 
-        function incrementWrongAnswer() {
+}
 
-        }
+function incrementWrongAnswer() {
 
-        function displayAdditionQuestion() {
+}
 
-        }
+function displayAdditionQuestion() {
 
-        function displaySubstractQuestion() {
+}
 
-        }
+function displaySubstractQuestion() {
 
-        function displayMultiplyQuestion() {}
+}
+
+function displayMultiplyQuestion() {}
