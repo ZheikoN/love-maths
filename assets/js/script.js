1ba0e9c9327c490cb109d320c45c3318ebb0d589
ZheikoN <negru.inger@gmail.com>
2022-05-24 21:07:03 +0000

    add addition function to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert(`you clicked ${gameType}`);
             } else {
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
@@ -21,13 +23,19 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     // creates two random numbers between 1-25;
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
-}
 
-runGame();
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }
+    else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
+}
 
 function checkAnswer() {
 
@@ -45,8 +53,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubstractQuestion() {
